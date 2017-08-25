25ea5bbdeac35e88f79c1ff41f7f914d6ff3227a
Thiago Fernando <thiago.fernando@msn.com>
2017-08-24 22:56:52 -0400

    Finalizado

--- ContratoDeTrabalho/ContratoDeTrabalho/Program.cs
+++ ContratoDeTrabalho/ContratoDeTrabalho/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Caelum.Stella.CSharp.Http;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caelum.Stella.CSharp.Format;
+using Caelum.Stella.CSharp.Vault;
 
 namespace ContratoDeTrabalho
 {
@@ -10,29 +14,49 @@ namespace ContratoDeTrabalho
     {
         static void Main(string[] args)
         {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+            var endereco = new ViaCEP();
             var contrato = new
             {
-                Empresa = "Alura Serviços Limitados",
-                Funcionario = "Mario",
-                Inicio = new DateTime(2018, 1, 1),
-                Cargo = "Encanador",
-                Salario = 2344.67,
+                Empresa = new
+                {
+                    RazaoSocial = "Alura Serviços Limitados",
+                    CNPJ = new CNPJFormatter().Format("24634654000135"),
+                    Endereco= endereco.GetEndereco("78015300"),
+                    Numero = "123 fundos",
+
+                },
+                Funcionario = new
+                {
+                    Nome= "Mario Luz",
+                    CPF = new CPFFormatter().Format("36372168251"),
+                    RG = "555555-00",
+                    Nacionalidade = "Italiano",
+                    EstadoCivil = "Casado",
+                    Cargo = "Encanador",
+                    Endereco = endereco.GetEndereco("78150322"),
+                    Numero = "234"
+                },
+                Inicio = new DateTime(2018, 1, 12).ToString("d"),
+                Salario = new Money(2344.67)
             };
             string documento = $@"============================CONTRATO INDIVIDUAL DE TRABALHO TEMPORÁRIO===============================================
 
-            EMPREGADOR: {contrato.Empresa}, com sede à(LOGRADOURO), (NUMERO), (BAIRRO), CEP(CEP), (LOCALIDADE), (UF), inscrita no CNPJ sob nº(CNPJ);
+            EMPREGADOR: {contrato.Empresa.RazaoSocial}, com sede à {contrato.Empresa.Endereco.Logradouro}, {contrato.Empresa.Numero}, {contrato.Empresa.Endereco.Bairro}, CEP {contrato.Empresa.Endereco.CEP}, 
+            {contrato.Empresa.Endereco.Localidade}, {contrato.Empresa.Endereco.UF}, inscrita no CNPJ sob nº{contrato.Empresa.CNPJ};
 
-            EMPREGADO: {contrato.Funcionario}, (NACIONALIDADE), (ESTADO CIVIL), portador da cédula de identidade R.G.nº(RG) e CPF/ MF nº(CPF), residente 
-            e domiciliado na(LOGRADOURO), (NUMERO), (BAIRRO), CEP(CEP), (LOCALIDADE), (UF).
+            EMPREGADO: {contrato.Funcionario.Nome}, {contrato.Funcionario.Nacionalidade}, {contrato.Funcionario.EstadoCivil}, portador da cédula de identidade R.G.nº{contrato.Funcionario.RG} e CPF/ MF nº {contrato.Funcionario.CPF}, residente 
+            e domiciliado na {contrato.Funcionario.Endereco.Logradouro}, {contrato.Funcionario.Numero}, {contrato.Funcionario.Endereco.Bairro}, {contrato.Funcionario.Endereco.CEP}
+            , {contrato.Funcionario.Endereco.Localidade}, {contrato.Funcionario.Endereco.UF}.
 
                 Pelo presente instrumento particular de contrato individual de trabalho, fica justo e contratado o seguinte:
 
             Cláusula 1ª - O EMPREGADO prestará ao EMPREGADOR, a partir de {contrato.Inicio} e assinatura deste instrumento, seus trabalhos exercendo a função de 
-            {contrato.Cargo}, prestando pessoalmente o labor diário no período compreendido entre 9 horas às 18 horas, e intervalo de 1 hora para almoço;
+             {contrato.Funcionario.Cargo}, prestando pessoalmente o labor diário no período compreendido entre 9 horas às 18 horas, e intervalo de 1 hora para almoço;
 
             Cláusula 2ª - Não haverá expediente nos dias de sábado, sendo prestado a compensação de horário semanal;
 
-            Cláusula 3ª - O EMPREGADOR pagará mensalmente, ao EMPREGADO, a título de salário a importância de {contrato.Salario} (SALÁRIO POR EXTENSO), com os 
+            Cláusula 3ª - O EMPREGADOR pagará mensalmente, ao EMPREGADO, a título de salário a importância de {contrato.Salario.ToString()}, ({contrato.Salario.Extenso()}) com os 
             descontos previstos por lei;
 
             Cláusula 4ª - Estará o EMPREGADO subordinado a legislação vigente no que diz respeito aos descontos de faltas e demais sanções disciplinares contidas 
@@ -45,14 +69,14 @@ namespace ContratoDeTrabalho
                 Como prova do acordado, assinam instrumento, afirmado e respeitando seu teor por inteiro, e firmam conjuntamente a este duas testemunhas, comprovando 
             as razões descritas.
 
-                (LOCALIDADE), (DATA POR EXTENSO)
+                {contrato.Empresa.Endereco.Localidade}, {DateTime.Today.ToString("D")}
 
 
             _______________________________________________________
-                {contrato.Empresa}
+                {contrato.Empresa.RazaoSocial}
 
             _______________________________________________________
-                {contrato.Funcionario}
+                {contrato.Funcionario.Nome}
 
             _______________________________________________________
                 (Nome, R.G, Testemunha)
@@ -61,8 +85,12 @@ namespace ContratoDeTrabalho
                 (Nome, R.G, Testemunha)
 
             ";
+            //StringBuilder sb = new StringBuilder();
+            //sb.AppendLine("Cargo Do Cara: " + contrato.Cargo + " Salario: " + contrato.Salario);
+            //sb.AppendLine("Empresa Do Cara: " + contrato.Empresa);
 
             Console.WriteLine(documento);
+            //Console.WriteLine(sb);
             Console.ReadKey();
         }
     }
